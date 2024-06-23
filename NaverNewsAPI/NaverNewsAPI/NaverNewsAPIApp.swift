21ffbbbe67a86f9ae36b2c96becb84e9538ddd48
WonyeolHEO <hyy96hyy@naver.com>
2024-06-23 21:14:43 +0900

    naverAPI initial Setting remove xcconfig

---
+++ NaverNewsAPI/NaverNewsAPI/NaverNewsAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NaverNewsAPIApp.swift
+//  NaverNewsAPI
+//
+//  Created by wonyoul heo on 6/23/24.
+//
+
+import SwiftUI
+
+@main
+struct NaverNewsAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
